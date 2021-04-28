c3d1c54b7d7cb9abb631a0e3f0713ae5ef8a2ad3
Richard Krejstrup <krejstrup@hotmail.com>
2021-04-28 16:45:46 +0200

    Lots of documentation and comments added and some cleanup

--- MVCAppAssignment2/Controllers/AJAXController.cs
+++ MVCAppAssignment2/Controllers/AJAXController.cs
@@ -14,7 +14,10 @@ namespace MVCAppAssignment2.Controllers
 
 
 
-
+        /// <summary>
+        /// Index loads the entire page with a view.
+        /// </summary>
+        /// <returns>The standard view to display.</returns>
         [HttpGet]
         public IActionResult Index()       // WORKS!!
         {
@@ -22,7 +25,10 @@ namespace MVCAppAssignment2.Controllers
         }
 
 
-
+        /// <summary>
+        /// AllPersons returns the list of all persons in the database/memory.
+        /// </summary>
+        /// <returns>The list of all the persons in the database/memory.</returns>
         [HttpGet]
         public IActionResult AllPersons() // WORKS!!
         {
@@ -32,6 +38,11 @@ namespace MVCAppAssignment2.Controllers
         }
 
 
+        /// <summary>
+        /// Filter is only using the person Id to filter out one person.
+        /// </summary>
+        /// <param name="Id">The person to look up.</param>
+        /// <returns>Returns a partial view of a person on success, otherwise NotFound (404).</returns>
         [HttpPost]
         public IActionResult Filter(int Id)
         {
@@ -41,12 +52,15 @@ namespace MVCAppAssignment2.Controllers
             {
                 return PartialView("_OnePersonPartial", onePerson);
             }
-
-
-            return NotFound(); // PartialView("_AllPersonsPartial", _myService.All());
+            return NotFound();
         }
 
 
+        /// <summary>
+        /// About opens up a partial view for the person clicked. The person partial view is replaced.
+        /// </summary>
+        /// <param name="Id">The Id pointing out the person to open about info with.</param>
+        /// <returns>Returns the partial view of a personal Card info.</returns>
         [HttpPost]
         public IActionResult About(int Id) // WORKS!!
         {
@@ -54,26 +68,34 @@ namespace MVCAppAssignment2.Controllers
 
             if (onePerson != null)
             {
-                //return PartialView("_personThumbnail", onePerson);
                 return PartialView("_PersonAboutPartial", onePerson);
             }
             return PartialView("_AllPersonsPartial", _myService.All());
         }
 
-
+        /// <summary>
+        /// NotAbout closes the info partial view for the person clicked. The person partial view is replaced.
+        /// </summary>
+        /// <param name="Id">The Id pointing out the person view to replace About with.</param>
+        /// <returns>Returns the standard partial view of a person.</returns>
         [HttpPost]
-        public IActionResult NotAbout(int Id) // WORKS??
+        public IActionResult NotAbout(int Id) // WORKS!!
         {
             Person onePerson = _myService.FindBy(Id);
 
             if (onePerson != null)
             {
-                //return PartialView("_personThumbnail", onePerson);
                 return PartialView("_PersonPartial", onePerson);
             }
             return PartialView("_AllPersonsPartial", _myService.All());
         }
 
+
+        /// <summary>
+        /// Remove deletes the given person from the database/memory. And deletes the representing personal row.
+        /// </summary>
+        /// <param name="Id">The Id of the person to be removed.</param>
+        /// <returns>Returns Ok (200) if success otherwise BadRequest or NotFound (404)</returns>
         [HttpPost]
         public IActionResult Remove(int Id) // WORKS!!
         {
@@ -90,17 +112,18 @@ namespace MVCAppAssignment2.Controllers
             }
 
             return BadRequest();
-            //return RedirectToAction(nameof(Index));
-
         }
 
 
 
-
+        /// <summary>
+        /// Edit is not quite finished yet!!!
+        /// </summary>
+        /// <param name="aPerson">The Person to be edited.</param>
+        /// <returns>Returns Ok (200) and data for the person Id on success, otherwise BadRequest or NotFound (404)</returns>
         [HttpPost]
         public IActionResult Edit(Person aPerson) // WORKS NOT QUITE YET!!
         {
-            //Person aPerson = _myService.FindBy(Id);
 
             if (aPerson == null)
             {
@@ -113,8 +136,6 @@ namespace MVCAppAssignment2.Controllers
             }
 
             return BadRequest();
-            //return RedirectToAction(nameof(Index));
-
         }
     }
 }
--- MVCAppAssignment2/wwwroot/js/PeopleJS.js
+++ MVCAppAssignment2/wwwroot/js/PeopleJS.js
@@ -15,8 +15,11 @@ deleteBtn.addEventListener("click", ajaxRemove);
 // jQuery has to be loaded!!
 
 
-// ==== Event Click part of the JavaScript ================
+// ==== Event Click part of the JavaScript: ================
 
+
+
+/// ajaxGetAll fetches the complete partial view for all persons.
 function ajaxGetAll(event) {
     if (event != null) event.preventDefault();
 
@@ -26,14 +29,16 @@ function ajaxGetAll(event) {
 
 }
 
+/// The ajaxFilter is called from an event from the button in Index view.
+/// The function posts the Id, fetched from the text-input, to the Controller.
+/// The response is checked for success and puts the resulting partial view 
+/// in the placeholder or signals the fails.
 function ajaxFilter(event) { 
     if (event != null) event.preventDefault();
 
     $.post("AJAX/Filter", {Id: idTexField.value},function (data, status) {
         if (status == "success") {
-            
             resultContainer.innerHTML = data;
-            
         }
 
     }).fail(function (jqXHR, textStatus, errorThrown) {
@@ -48,6 +53,9 @@ function ajaxFilter(event) {
 
 }
 
+/// this function is called from an event from the button in Index view.
+/// ajaxRemove Posts the Id, fetched from the text-input, to the Controller
+/// The function checks for any success or fails.
 function ajaxRemove(element, event) {
     if (event != null) event.preventDefault();
 
@@ -73,35 +81,37 @@ function ajaxRemove(element, event) {
 
 
 
+// ==== OnClick part of the JavaScript: ========================
 
 
-// ==== OnClick part of the JavaScript ========================
-
-
-function About(id, event) {     // Ask for more details from person view
+/// About is called from the button in the end action end of the person listing.
+/// Sends the person Id to be shown to this function and pass it on to the Controller. 
+/// THe controller looks up a new partial view and returns it here.
+/// The partial person view is replacing the standard person partial view.
+function About(id, event) {
     if (event != null) event.preventDefault();
 
     $.post("AJAX/About", { Id: id }, function (data, status) {
 
-        //resultContainer.innerHTML = data;
         $("#per" + id).replaceWith(data);
     });
 
 }
 
-function CloseAbout(id, event) {     // Just closes the about info partial
+
+/// Just closes the about info partial by the same principal as for About.
+///
+function CloseAbout(id, event) {     
     if (event != null) event.preventDefault();
 
     $.post("AJAX/NotAbout", { Id: id }, function (data, status) {
         $("#cab" + id).replaceWith(data);
     });
-
-    //$("#cab" + id).remove();
 }
 
 /// DeletePerson is called from the button in the end of the Person listing
-/// And then send the person data to remove to backend
-/// How do I put that Id into a person data model to the backend?
+/// And then send the person data to remove to backend.
+///
 function DeletePerson(PersonId, event) {
     if (event != null) console.log("Delete person");
 
@@ -115,5 +125,7 @@ function DeletePerson(PersonId, event) {
     });;
 }
 
+///====== All other bits and bytes: ===========================
 
-ajaxGetAll();   // Load up the person-list in the view
+//== Load up the person-list in the view:
+ajaxGetAll();   
